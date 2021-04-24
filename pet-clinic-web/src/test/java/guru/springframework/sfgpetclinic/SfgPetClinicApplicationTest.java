6dab81c16e0188689a2f4eca32e9eea8a70ddf3e
Steven Lu <steven@swaminc.com>
2021-04-24 17:46:23 -0400

    committed

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SfgPetClinicApplicationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
