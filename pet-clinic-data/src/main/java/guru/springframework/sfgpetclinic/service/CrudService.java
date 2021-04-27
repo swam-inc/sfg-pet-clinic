1c3bb8698f6edf1e98e45363af8a3ee36061b541
Steven Lu <steven@swaminc.com>
2021-04-27 16:36:51 -0400

    refactoring to common crud interface. closes #3

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/CrudService.java
@@ -5,7 +5,7 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import java.util.Set;
 
 public interface  CrudService<T, ID> {
-    T findById( ID idField);
+    T findById( ID id);
     Set<T> findAll();
     T save(T object);
     void delete(T object);
