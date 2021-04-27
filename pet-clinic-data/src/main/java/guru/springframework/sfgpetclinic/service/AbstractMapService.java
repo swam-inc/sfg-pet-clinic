632f15ea535fdbe62611abf99ee002703c6b362c
Steven Lu <steven@swaminc.com>
2021-04-27 17:53:50 -0400

    Implemented Map Based Services. Close #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/AbstractMapService.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.service;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService <T, ID>{
+    protected Map<ID, T> map = new HashMap<>();
+
+    T findById( ID id){
+        return map.get(id);
+    }
+    Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+    T save(ID id, T object){
+        map.put(id, object);
+        return object;
+    }
+    void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+    void deleteById(ID id){
+        map.remove(id);
+    }
+}
