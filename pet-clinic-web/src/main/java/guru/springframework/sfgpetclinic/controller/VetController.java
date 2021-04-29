4fbfa5c084b74b4047fad1c14351ba5ed4c56f65
Steven Lu <steven@swaminc.com>
2021-04-28 22:33:24 -0400

    Implemented Vet Index Page and Web View Controller. Close #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vet", "/vet/index", "/vet/index.html"})
+    public String vetIndex(){
+        return "vet/index";
+    }
+}
