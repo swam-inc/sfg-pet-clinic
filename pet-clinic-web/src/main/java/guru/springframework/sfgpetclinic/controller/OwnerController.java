25f3106dba80a1d964dc51f2d7a58dff7c529319
Steven Lu <steven@swaminc.com>
2021-04-30 18:54:22 -0400

    Implemented display vet and owner in website Close #10

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.map.OwnerServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+  private final OwnerServiceMap ownerServiceMap;
+
+  public OwnerController(OwnerServiceMap ownerServiceMap) {
+    this.ownerServiceMap = ownerServiceMap;
+  }
+
   @RequestMapping({"/owner", "/owner/index", "/owner/index.html"})
-    public String getIndex(){
-      return "owner/index";
+    public String getIndex(Model model){
+      model.addAttribute("owners", ownerServiceMap.findAll());
+    return "owner/index";
   }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.map.VetServiceMap;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetServiceMap vetServiceMap;
+
+    public VetController(VetServiceMap vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
     @RequestMapping({"/vet", "/vet/index", "/vet/index.html"})
-    public String vetIndex(){
+    public String vetIndex(Model model){
+        model.addAttribute("vets", vetServiceMap.findAll());
         return "vet/index";
     }
 }
