106188335358ae9867d20916b133455a4a0013e5
Steven Lu <steven@swaminc.com>
2021-05-07 21:06:08 -0400

    add JPA annotation to base Entity Close #17

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,6 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.hibernate.annotations.Generated;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
